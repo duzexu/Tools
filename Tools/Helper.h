6bc1c19d0da004b93b5fc8b37bdb9d88e696676f
duzexu <zexu007@qq.com>
2014-01-23 17:49:05 +0800

    宏定义添加

--- Tools/Helper.h
+++ Tools/Helper.h
@@ -86,3 +86,17 @@ static CGAffineTransform CGAffineTransformMakeRotationAt(CGFloat angle, CGPoint 
  *  @brief  得到UUID
  */
 #define GetUUID [[[UIDevice currentDevice] identifierForVendor] UUIDString]
+
+/**
+ *  @brief  输出
+ */
+#define NSLog(format, ...)   fprintf(stderr, "<%s : %d> %s\n",                                           \
+[[[NSString stringWithUTF8String:__FILE__] lastPathComponent] UTF8String],  \
+__LINE__, __func__);                                                        \
+(NSLog)((format), ##__VA_ARGS__);                                           \
+fprintf(stderr, "-------\n");
+#define NSLogRect(rect) NSLog(@"%s x:%.4f, y:%.4f, w:%.4f, h:%.4f", #rect, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
+#define NSLogSize(size) NSLog(@"%s w:%.4f, h:%.4f", #size, size.width, size.height)
+#define NSLogPoint(point) NSLog(@"%s x:%.4f, y:%.4f", #point, point.x, point.y)
+
+
