578538aa0ff6671b698f9582a92537b489298479
duzexu <zexu007@qq.com>
2014-09-03 15:00:50 +0800

    一部图片加载和波浪刷新效果添加

---
+++ Tools/Wave/UITableView+Wave.h
@@ -0,0 +1,18 @@
+/**
+ *  给tableView刷新数据添加波浪效果
+ */
+#define kBOUNCE_DISTANCE  4.f
+#define kWAVE_DURATION   .5f
+
+
+typedef NS_ENUM(NSInteger,WaveAnimation) {
+    LeftToRightWaveAnimation = -1,
+    RightToLeftWaveAnimation = 1
+};
+
+
+@interface UITableView (Wave)
+
+- (void)reloadDataAnimateWithWave:(WaveAnimation)animation;
+
+@end
